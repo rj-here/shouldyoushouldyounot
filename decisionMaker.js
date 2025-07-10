fedc2d1bf60d2dcd53c816b837339174f76d72ce
rj-here <r1sh1j@my.yorku.ca>
2025-07-10 19:50:32 -0400

    Making progress

--- decisionMaker.js
+++ decisionMaker.js
@@ -1,20 +1,20 @@
 function takePoints() {
 alert("WARNING: This is to help you make a decision, easing your troubles. BUT, DO NOT FULLY GO WITH THE DECISION MADE HERE!");
-var decisionPoints = 0;
-var decision = prompt("Do you have a point to make? (Yes or No)");
-while (decision == "yes" || decision == "Yes") {
+var decisionPoints = 0; //Initialize the decision points
+var decision = prompt("Do you have a point to make? (Yes or No)"); //Add a point for making the decision
+while (decision == "yes" || decision == "Yes") { //Loop running until user says no
     var point = document.getElementById("point").value;
     var pointValue = parseInt(document.getElementById("pointValue").value);
-    decisionPoints += pointValue;
-    decision = prompt("Do you have a point to make? (Yes or No)")
+    decisionPoints += pointValue; //Adding the "value" of the point for/against the decision
+    decision = prompt("Do you have a point to make? (Yes or No)"); //Keep going until the user wishes to stop
 }
-if (decisionPoints > 0) {
+if (decisionPoints > 0) {//Positive -> Good (probably)
     alert("The points raised in making your decision are: " + decisionPoints + " points.\nThis sounds like a good decision to make. Ultimately, the decision is yours!");
 }
-else if (decisionPoints < 0) {
+else if (decisionPoints < 0) {//Negative -> Bad (probably)
     alert("The points raised in making your decision are: " + decisionPoints + " points.\nThis doesn't sound like a good decision to make. Ultimately, the decision is yours!");
 }
-else if (decisionPoints == 0) {
+else if (decisionPoints == 0) {//Neutral -> On you, but then back to square one!
     alert("The points raised in making your decision are: " + decisionPoints + " points.\nThis sounds like a decision, which if made is okay, and if not made, is also okay. It's not the end of the world and ultimately, the decision is yours!");
 }
 }
